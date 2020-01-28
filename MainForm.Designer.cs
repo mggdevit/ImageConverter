86cb4343d79b6d0d1174f0079dd57bb7aefa8663
moneymakermgg <38669075+moneymakermgg@users.noreply.github.com>
2020-01-28 19:08:05 +0300

    begin

---
+++ MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ImageConverter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FolderPeeker = new System.Windows.Forms.FolderBrowserDialog();
+            this.lPath = new System.Windows.Forms.Label();
+            this.btChangeFolder = new System.Windows.Forms.Button();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.cbFormats = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btConvert = new System.Windows.Forms.Button();
+            this.lStatus = new System.Windows.Forms.Label();
+            this.cbRewriteFiles = new System.Windows.Forms.CheckBox();
+            this.cbIgnoreErrors = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lPath
+            // 
+            this.lPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lPath.Location = new System.Drawing.Point(13, 43);
+            this.lPath.Name = "lPath";
+            this.lPath.Size = new System.Drawing.Size(553, 35);
+            this.lPath.TabIndex = 0;
+            this.lPath.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btChangeFolder
+            // 
+            this.btChangeFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btChangeFolder.Location = new System.Drawing.Point(573, 43);
+            this.btChangeFolder.Name = "btChangeFolder";
+            this.btChangeFolder.Size = new System.Drawing.Size(189, 35);
+            this.btChangeFolder.TabIndex = 1;
+            this.btChangeFolder.Text = "Folder...";
+            this.btChangeFolder.UseVisualStyleBackColor = true;
+            this.btChangeFolder.Click += new System.EventHandler(this.btChangeFolder_Click);
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(13, 144);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(749, 23);
+            this.pBar.TabIndex = 3;
+            this.pBar.Visible = false;
+            // 
+            // cbFormats
+            // 
+            this.cbFormats.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbFormats.FormattingEnabled = true;
+            this.cbFormats.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.cbFormats.Location = new System.Drawing.Point(478, 94);
+            this.cbFormats.Name = "cbFormats";
+            this.cbFormats.Size = new System.Drawing.Size(284, 28);
+            this.cbFormats.TabIndex = 4;
+            this.cbFormats.SelectionChangeCommitted += new System.EventHandler(this.cbFormats_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(370, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Convert to ...";
+            // 
+            // btConvert
+            // 
+            this.btConvert.Enabled = false;
+            this.btConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btConvert.Location = new System.Drawing.Point(573, 188);
+            this.btConvert.Name = "btConvert";
+            this.btConvert.Size = new System.Drawing.Size(189, 47);
+            this.btConvert.TabIndex = 6;
+            this.btConvert.Text = "Convert";
+            this.btConvert.UseVisualStyleBackColor = true;
+            this.btConvert.Click += new System.EventHandler(this.btConvert_Click);
+            // 
+            // lStatus
+            // 
+            this.lStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lStatus.Location = new System.Drawing.Point(12, 170);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(211, 29);
+            this.lStatus.TabIndex = 7;
+            this.lStatus.Visible = false;
+            // 
+            // cbRewriteFiles
+            // 
+            this.cbRewriteFiles.AutoSize = true;
+            this.cbRewriteFiles.Checked = true;
+            this.cbRewriteFiles.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbRewriteFiles.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cbRewriteFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.cbRewriteFiles.Location = new System.Drawing.Point(13, 81);
+            this.cbRewriteFiles.Name = "cbRewriteFiles";
+            this.cbRewriteFiles.Size = new System.Drawing.Size(262, 23);
+            this.cbRewriteFiles.TabIndex = 8;
+            this.cbRewriteFiles.Text = "Overwrite files with the same name";
+            this.cbRewriteFiles.UseVisualStyleBackColor = true;
+            // 
+            // cbIgnoreErrors
+            // 
+            this.cbIgnoreErrors.AutoSize = true;
+            this.cbIgnoreErrors.Checked = true;
+            this.cbIgnoreErrors.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbIgnoreErrors.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cbIgnoreErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.cbIgnoreErrors.Location = new System.Drawing.Point(13, 110);
+            this.cbIgnoreErrors.Name = "cbIgnoreErrors";
+            this.cbIgnoreErrors.Size = new System.Drawing.Size(203, 23);
+            this.cbIgnoreErrors.TabIndex = 9;
+            this.cbIgnoreErrors.Text = "Ignore convertation errors";
+            this.cbIgnoreErrors.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 246);
+            this.Controls.Add(this.cbIgnoreErrors);
+            this.Controls.Add(this.cbRewriteFiles);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.btConvert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbFormats);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.btChangeFolder);
+            this.Controls.Add(this.lPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Image Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog FolderPeeker;
+        private System.Windows.Forms.Label lPath;
+        private System.Windows.Forms.Button btChangeFolder;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.ComboBox cbFormats;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btConvert;
+        private System.Windows.Forms.Label lStatus;
+        private System.Windows.Forms.CheckBox cbRewriteFiles;
+        private System.Windows.Forms.CheckBox cbIgnoreErrors;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using ImageMagick;
+
+namespace ImageConverter
+{
+    public partial class MainForm : Form
+    {
+        private string source_dir = String.Empty; 
+        private string dest_dir = String.Empty;
+        private string image_format = String.Empty;
+        private List<string> image_formats = new List<string>();
+        List<string> files = new List<string>();
+        List<string> dirs = new List<string>();
+        int convert_progress = 1;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            foreach(var item in Enum.GetValues(typeof(MagickFormat)))
+            {
+                image_formats.Add(item.ToString().ToLower());
+                cbFormats.Items.Add(item);
+            }
+            pBar.Minimum = 0;
+            pBar.Maximum = 2;
+            pBar.Step = 1;
+            pBar.Value = 1;
+        }
+
+        private void btChangeFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowser.ShowDialog();
+                if(result == DialogResult.OK)
+                {
+                    lPath.Text = source_dir = folderBrowser.SelectedPath;
+                    dest_dir = source_dir + "\\Convert";
+                    GetFileTree(source_dir);
+                    pBar.Maximum = files.Count;
+                }
+            }
+            CheckControlState();
+        }
+
+        private void CheckControlState()
+        {
+            btConvert.Enabled = !String.IsNullOrEmpty(source_dir) && cbFormats.SelectedIndex >= 0;
+        }
+
+        private void GetFileTree(string Path)
+        {
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(Path))
+                {
+                    if (dir.Contains("Convert"))
+                        continue;
+                    dirs.Add(dir);
+                    foreach (string file in Directory.GetFiles(dir))
+                    {
+                        files.Add(file);
+                    }
+                    GetFileTree(dir);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(String.Format("Can't find files: {0}", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ConvertFile(string file)
+        {
+            try
+            {
+                string filename = file.Split('\\')[file.Split('\\').Length - 1].Split('.')[0];
+                string fileext = file.Split('\\')[file.Split('\\').Length - 1].Split('.')[1].ToLower();
+                if (!image_formats.Contains(fileext) && !cbIgnoreErrors.Checked)
+                {
+                    throw new Exception("Not supportable image format");
+                }
+                string result_filepath = dest_dir + "\\" + filename + "." + image_format;
+                if (File.Exists(result_filepath))
+                {
+                    if (!cbRewriteFiles.Checked)
+                    {
+                        return;
+                    }
+                    File.Delete(result_filepath);
+                }
+                using (MagickImage image = new MagickImage(file))
+                {
+                    image.Format = (MagickFormat)Enum.Parse(typeof(MagickFormat), image_format);
+                    image.Write(result_filepath);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                if (!cbIgnoreErrors.Checked)
+                    MessageBox.Show(String.Format("Can't convert file: {0}", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            UpdateControl();
+        }
+
+        delegate void Progress(int value);
+
+        private void UpdateControl()
+        {
+            lStatus.Invoke(new MethodInvoker(delegate
+            {
+                convert_progress++;
+                lStatus.Text = convert_progress.ToString() + " из " + files.Count.ToString();
+                lStatus.Update();
+            }));
+            pBar.Invoke(new MethodInvoker(delegate
+            {
+                pBar.PerformStep();
+            }));
+        }
+           
+        private async Task ConvertAsync()
+        {
+            lStatus.Text = "Processing...";
+
+            if (!Directory.Exists(dest_dir))
+            {
+                Directory.CreateDirectory(dest_dir);
+            }
+
+            List<Task> tasks = new List<Task>();
+            foreach(var file in files)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    ConvertFile(file);
+                }));
+            }
+            await Task.WhenAll(tasks.ToArray());
+        }
+
+        private async void btConvert_Click(object sender, EventArgs e)
+        {
+            pBar.Visible = lStatus.Visible = true;
+            btConvert.Enabled = false;
+            btConvert.Text = "Processing...";
+            await ConvertAsync();
+            btConvert.Enabled = true;
+            btConvert.Text = "Convert";
+            pBar.Visible = lStatus.Visible = false;
+            pBar.Value = 1;
+            convert_progress = 1;
+        }
+
+        private void cbFormats_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            image_format = cbFormats.SelectedItem.ToString();
+            CheckControlState();
+        }
+    }
+}
